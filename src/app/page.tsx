1a0cb3fc3b95c9c93669c120b7586adbf672df2f
Daniil Zaretskiy <zaretskiy03@gmail.com>
2023-09-25 01:15:07 +0300

    configured base config and global styles

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { ResetCSSGlobalStyle } from "@/styles/reset"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>HOME</h1>
+      <ResetCSSGlobalStyle></ResetCSSGlobalStyle>
+    </div>
+  )
+}
---
+++ src/styles/reset.ts
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from "styled-components";
+
+export const ResetCSSGlobalStyle = createGlobalStyle`
+/* Задаем box-sizing: border-box, чтобы отступы не влияли на размеры элементов */
+*, *::before, *::after {
+	box-sizing: border-box;
+}
+
+
+
+html,
+body {
+  max-width: 1920px;
+  max-height: 1080px;
+  width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+  font-size: 16px;
+}
+
+body {
+  margin: 0;
+}
+
+[tabindex='-1']:focus {
+  outline: none;
+}
+
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: "Open Sans", sans-serif;
+}
+
+`;
