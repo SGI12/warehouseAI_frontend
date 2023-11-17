0226c6aa2a4d09224fb1a67ae9066f1c3687cc69
Daniil Zaretskiy <zaretskiy03@gmail.com>
2023-11-17 23:21:14 +0300

    added searchfield to favorites

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -19,17 +19,61 @@ import { useEffect, useState } from "react";
 
 const FavoritesPage = () => {
     const router = useRouter()
-    const filterValues:Array<string> = ['По популярности', 'По оценкам',]
+    const filterValues:Array<string> = ['По популярности',]
     const [isFilterOpen, setFilterOpen] = useState(false)
     const [activeIndex, setActiveIndex] = useState(-1)
     const [isLoading, setLoading] = useState(true);
     const {user} = useUserContext()
+    const [searchParams, setSearchParams] = useState('');
     const [FavoriteAIData, setAIData] = useState<Array<any>>([])
+    const [isSearchEmpty, setSearchEmpty] = useState(false)
+    
+  
     const filterHandler = (e:React.MouseEvent<HTMLButtonElement>) => {
         e.stopPropagation();
         setFilterOpen(!isFilterOpen)
     }
-    
+    const SortHandler = (e:any) => { 
+
+        if (activeIndex !== -1) {
+            getUserFavoriteAI().then((res) => {
+                setAIData(res.data?.favorite_ai)
+            })
+            
+        }
+        else {
+            
+            if (e.target.id === 'По популярности') {
+                FavoriteAIData.sort((a, b) => b.used - a.used)
+             }
+        }    
+    }
+
+    const SearchHandler = (e:any) => {
+        
+        setSearchParams(e.target.value)
+        const SearchAIData = FavoriteAIData.filter((word) => {
+            console.log(word.name)
+            return `${word.name}`.toLowerCase().includes((e.target.value).toLowerCase())
+            
+        })
+        if (!e.target.value) {
+            setActiveIndex(-1)
+            getUserFavoriteAI().then((res) => {
+            setAIData(res.data?.favorite_ai)
+            })
+        }
+        if (SearchAIData.length !== 0) {
+            setSearchEmpty(false)
+            setAIData(SearchAIData)
+        }
+            
+        else {
+            setActiveIndex(-1)
+            setSearchEmpty(true)
+        }
+
+    }
     useEffect(() => {
     const fetchData = () => {
         
@@ -38,17 +82,17 @@ const FavoritesPage = () => {
            .then((res) => {
               setAIData(res.data?.favorite_ai)
               setTimeout(() => setLoading(false), 1000)
-
-              
            })
            .catch((e) => {
                console.log(e.response?.status)
            })
        }
         
-            check().then(() =>{
+            check()
+            .then(() =>{
                 user.setIsAuth(true)
                 getAIData()
+                
             })
             .catch((err) => {
                 {
@@ -62,7 +106,10 @@ const FavoritesPage = () => {
             })  
     }
     fetchData();
+  
     },[user]);
+
+    
     
     if (isLoading) {
         return <Loader/>
@@ -73,24 +120,27 @@ const FavoritesPage = () => {
             <Header/>
             <H1WithPadding color="#ffffff">Избранное</H1WithPadding>
             <SearchAndFilterContainer>
-                <SearchFieldFavorites placeholder="Искать в избранном"/>
+                <SearchFieldFavorites onChange={(e:any) => SearchHandler(e)} value={searchParams} placeholder="Искать в избранном"/>
                 <FilterContainer>
                     <FilterButton onClick={e => {filterHandler(e)}}  className={isFilterOpen ? 'pressed' : 'none'}><FilterIcon isOpen={isFilterOpen}/>
                     Фильтр
                     </FilterButton>
                     {isFilterOpen && 
                         <FilterMenuContainer className={isFilterOpen ? 'open' : 'close'}>
-                             {filterValues.map((value,index) => <FilterElement onClick={() => setActiveIndex(index)} id={value} className={activeIndex==index ? 'active' : ''} key={index}>{value}</FilterElement>)}
+                             {filterValues.map((value,index) => <FilterElement onClick={(e) => {
+                                activeIndex == index ? setActiveIndex(-1) : setActiveIndex(index)
+                                SortHandler(e)
+                            }} id={value} className={activeIndex==index ? 'active' : ''} key={index}>{value}</FilterElement>)}
                         </FilterMenuContainer>
                     }
                 </FilterContainer>
             </SearchAndFilterContainer>
-            {FavoriteAIData.length==0 && <H2Styled color="#ffffff">Вы пока не добавили ни одной нейросети в избранное.</H2Styled>}
+            {(FavoriteAIData.length==0 || isSearchEmpty) && <H2Styled color="#ffffff">Ничего не найдено.</H2Styled>}
             <FavoritesAIContainer>
-            <FavoritesAiGrid>  
+            {!isSearchEmpty && <FavoritesAiGrid>  
             {FavoriteAIData.map((props, index) => <AiCard key={index} props={...props}/>)}
             
-            </FavoritesAiGrid>
+            </FavoritesAiGrid>}
             </FavoritesAIContainer>
             <Footer/>
         </FavoritesPageMainContainer>
